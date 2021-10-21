9eeac61ec7a24fbaf1dcd0ed14b07f5538da791d
Genius2008 <92729859+Genius2008@users.noreply.github.com>
2021-10-21 19:36:08 +0300

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,68 +1,60 @@
 //
 //  main.swift
-//  lessonTestOne
+//  Homework2
 //
-//  Created by Евгения Шуляковская on 13.10.2021.
+//  Created by Евгения Шуляковская on 18.10.2021.
 //
 
 import Foundation
 
 print("Hello, World!")
 
+// 1. Четное число или нет
 
-
-// Решить квадратное уравнение
-
-let a: Double = 1
-let b: Double = 2
-let c: Double = 3
-let x1: Double
-let x2: Double
-
-var D: Double = b * b - 4 * a * c
-
-if D < 0 {
-    print ( "корней нет")
-}
-else if D == 0 {
-    x1 = ( -b) / ( 2 * a)
-    print ( "x = " , x1)
-}
-else if D > 0 {
-    x1 = ( -b + sqrt (D)) / (2 * a)
-    x2 = ( -b - sqrt (D)) / ( 2 * a)
-    print ( x1, x2 )
+func findNumber ( number: Int){
+    number % 2 == 0 ? print ("четное"): print ("нечетное")
 }
 
- //Найти площадь,периметр и гипотенузу треугольника
 
-let catet1 = r
-let catet2 = y
-let r: Double = 12
-let y: Double = 8
-
-let ploscad = S
-let S: Double = (r * y) / 2
-print (S)
-
-let hypotenuse = H
-let H: Double = sqrt(pow (r, 2) + pow (y, 2))
-print (H)
-
-let perimetr = P
-let P: Double = r + y + H
-print (P)
+// 2. делится на 3 без остатка
+func devideNumber ( number: Int){
+    number % 3 == 0 ? print ("четное"): print ("нечетное")
+}
 
 
+// 3. Создать возрастающий массив из 100
 
-// Найти сумму вклада
+var hundredNumbers = [Int] ()
+    for number in 1...100{
+    hundredNumbers.append(number)
+}
+print (hundredNumbers)
 
-let deposit = d
-let d: Float =  100
-let percent = p
-let p: Float = 15
-let years = y
-let y: Float = 5
-let sum: Float = (d * p) / 100
-let sum5: Float = (sum * 5) + d
-print (sum5)
+
+// 4. Удалить из массива все четные числа и числа, которые не делятся на 3
+
+var hundredNumbers = [Int] ()
+    for number in 1...100{
+    hundredNumbers.append(number)
+}
+for remuveNumbers in hundredNumbers{
+    remuveNumbers % 2 == 0 || remuveNumbers % 3 == 0 :
+    print ("removeNumbers")
+}
+
+
+// 5. Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 50 элементов.
+
+func fibonacci(_n: Int) → [Int] {
+    var _: [Int] = [0, 1]
+    
+    for sum in 2...n {
+        fibonacciNumbers.append(fibonaccinumbers[sum -1] + fobonacciNumbers[sum - 2])
+    }
+    return fibonacciNumbers
+
+}
+print(fibonacci(50))
+
+
+
